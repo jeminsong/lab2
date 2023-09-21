448ae9d614788e2f51f736982e92f746ccaa73e3
jeminsong <143300705+jeminsong@users.noreply.github.com>
2023-09-20 20:59:24 -0700

    Update madlib.cc
    Changed the Google Style Format

--- part-2/madlib.cc
+++ part-2/madlib.cc
@@ -36,26 +36,20 @@ int main(int argc, char const *argv[]) {
  // Print "Computers have many ", plural_noun, " which gives them the
  // advantage\n"
  // Print "over people for many computational tasks.\n"
-    std::string singular_noun = "singular noun";
-    std::string plural_noun = "plural noun";
-    std::string present_tense_verb = "present tense verb";
-    std::cout << "Please enter a singular noun: ";
-    std::cin >> singular_noun;
-    std::cout << "Please enter a plural noun: ";
-    std::cin >> plural_noun;
-    std::cout << "Please enter a present tense verb: ";
-    std::cin >> present_tense_verb;
-    std::cout << "\n";
-    std::cout << "Computers are small enough to fit into a "
-              << singular_noun << ".\n";
-    std::cout << "Computers can add, multiply, divide, and "
-              << present_tense_verb << ".\n";
-    std::cout << "People can "
-              << present_tense_verb
-              << " too but computers do it better.\n";
-    std::cout << "Computers have many "
-              <<plural_noun
-              << " which gives them the advantage.\n";
-    std::cout << "over people for many computational tasks.\n";
-    return 0;
+ std::string singular_noun = "singular noun";
+ std::string plural_noun = "plural noun";
+ std::string present_tense_verb = "present tense verb";
+ std::cout << "Please enter a singular noun: ";
+ std::cin >> singular_noun;
+ std::cout << "Please enter a plural noun: ";
+ std::cin >> plural_noun;
+ std::cout << "Please enter a present tense verb: ";
+ std::cin >> present_tense_verb;
+ std::cout << "\n";
+ std::cout << "Computers are small enough to fit into a " << singular_noun << ".\n";
+ std::cout << "Computers can add, multiply, divide, and " << present_tense_verb << ".\n";
+ std::cout << "People can " << present_tense_verb << " too but computers do it better.\n";
+ std::cout << "Computers have many " <<plural_noun << " which gives them the advantage.\n";
+ std::cout << "over people for many computational tasks.\n";
+ return 0;
 }
